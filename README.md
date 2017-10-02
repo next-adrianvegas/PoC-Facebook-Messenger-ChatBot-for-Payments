e9967757f14fa62d1ae0b64e10d945dba3bc6229
adrian.vegas <adrian.vegas@bbva.com>
2017-10-02 09:27:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<img src="icon.png" align="right" />
+
+# PoC Facebook Messenger ChatBot for Payments
+
+In this PoC i build a **simple chatBot for Facebook Messenger** that interact with the objetive of **make a pay** instead the chat . 
+
+You can diploy this app on Heroku and connect to facebook messenger by a webhook, in Facebook developers page you can find all you need to create your own account as a developer and configure the webhook for a chat bot :  [Facebook developer Guide][1]
+
+<i class="icon-cog"></i>##How works?
+
+Currently this bot shows the options: 
+Integration with Paypal or Stripe for payment through Messenger is currently in beta and only available in the US.
+
+
+[1]: https://developers.facebook.com/docs/messenger-platform/getting-started
---
+++ index.js
@@ -0,0 +1,171 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+//Setup
+// App Secret can be retrieved from the App Dashboard
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = process.env.VALIDATION_TOKEN
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hi!')
+})
+
+// for Facebook verification
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          console.log("1 Normal message");
+          receivedMessage(event);
+        }else if(event.postback){
+          console.log("1 Postback");
+          receivedPostback(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    /*switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+      case 'Hola':
+          sendTextMessage(senderID, 'Hola , ¿Qué tal?');
+          break;
+      default:
+        sendGenericMessage(senderID);
+    }*/
+    sendOptionsMessage(senderID);
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var payload = event.postback.payload;
+
+  console.log("SenderID : " + senderID);
+
+  console.log("Postback : "+payload);
+  switch (payload) {
+      case 'VER_PAGOS_PENDIENTES':
+          sendTextMessage("Pagos pendientes");
+        break;
+      case 'VER_HISTORICO_PAGOS' : 
+          sendTextMessage(senderID , 'Historico');
+          break;
+      default:
+        sendTextMessage(senderID, payload);
+    }
+
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+  callSendAPI(recipientId, messageData);
+}
+
+function callSendAPI(recipientId , messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+    }
+  });  
+}
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'))
+})
+
+
+
